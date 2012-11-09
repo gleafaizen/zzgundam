fd418760d0b313d5fe852d4d6caa32d3c74a362d
2341006 <ryoswarld@gmail.com>
2012-11-09 16:02:24 +0900

    とりあえず

---
+++ script/list.js
@@ -0,0 +1,19 @@
+$(function(){
+    $.ajax({
+    url: 'ra-men.xml',
+    type: 'GET',
+    dataType: 'xml',
+    timeout: 1000,
+    error: function(){
+        alert("request timed out. try again!");
+    },
+    success: function(xml){
+    $(xml).find('J').each(function(){
+        var title = $(this).find('TITLE').text();
+        $('<li></li>')
+            .title;
+            .appendTo('ul');
+       });
+   }
+   });
+})
