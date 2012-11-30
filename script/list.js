8f7174857a0cf4f0635a9c8dd791bbdd5479ec7e
2341006 <ryoswarld@gmail.com>
2012-11-30 14:22:55 +0900

    kousinn

--- script/list.js
+++ script/list.js
@@ -8,13 +8,14 @@ $(function(){
         alert("request timed out. try again!");
     },
     success: function(xml){
-    $('.firstChild').remove();
+    var ul = $('#taglist');
+    //$('#taglist').remove();
     $(xml).find('J').each(function(){
         var title = $(this).find('TITLE').text();
         var address = &(this).find('Address').text();
         $('<li></li>')
            .html(title + address + "<br/>")
-            .appendTo('ul');
+            .appendTo(ul);
        });
    }
    });
